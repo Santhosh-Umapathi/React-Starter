62b5ed35f57dbf0b44bfa09b22c55d752bcb3ea0
Santhosh Umapathi <Santhosh.professional1@gmail.com>
2020-09-06 18:32:33 +0200

    css vs inline, props, state, class , functional

--- src/App.js
+++ src/App.js
@@ -1,14 +1,64 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
+
+
 
 class App extends Component
 {
-  render() {
+  state = {
+    persons: [
+      { name: 'Max', age: 25 },
+      {name: 'Jack', age: 30},
+      {name: 'Kol', age: 49},
+    ],
+    otherState: 'untouched'
+  }
+
+  switchNamesHandler = () =>
+  {
+    this.setState({
+      persons: [
+        { name: 'Maximillian', age: 25 },
+        {name: 'Jackian', age: 30},
+        {name: 'Kolian', age: 49},
+      ],
+    })
+    }
+
+    nameChangeHandler = (event) =>
+    {
+      this.setState({
+        persons: [
+          { name: event.target.value, age: 25 },
+          {name: 'Jackian', age: 30},
+          {name: 'Kolian', age: 49},
+        ]
+      })
+    }
+
+  render()
+  {
+    const style = {
+      backgroundColor: 'orange',
+      border: '1px solid blue',
+      cursor: 'pointer',
+      padding:'8px'
+    }
+
+
+
     return (
       <div className="App">
-        <p className="App-intro">
-          React
-        </p>
+        <p className="App-intro"> React Starter</p>
+        <button onClick = {this.switchNamesHandler} style={style}> 
+          Switch Names
+        </button>
+
+        <Person name={this.state.persons[0].name}/>
+        <Person />
+        <Person name={this.state.persons[2].name} inputHandler={this.nameChangeHandler}/>
+
       </div>
     );
   }
---
+++ src/Person/Person.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Person.css'
+
+const Person = (props) =>
+{
+
+	const {name, age, children, inputHandler} = props
+
+	return (
+	<div>
+		<p className = 'Person'> I am {name}, {age} years old </p>
+			<p>{children}</p>
+			<input onChange={inputHandler} />
+	</div>
+); 
+}
+
+export default Person;
