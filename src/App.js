24c8be08f6b5e8c526eb95ab4de9dbe703cc116c
Santhosh Umapathi <Santhosh.professional1@gmail.com>
2020-09-12 11:33:57 +0200

    css dynamic classes using array and join

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
-import UserInput from './UserInput/UserInput';
-import UserOutput from './UserOutput/UserOutput';
-import Validation from './Validation/Validation'
-import CharComp from './Validation/CharComp'
+// import UserInput from './UserInput/UserInput';
+// import UserOutput from './UserOutput/UserOutput';
+// import Validation from './Validation/Validation'
+// import CharComp from './Validation/CharComp'
 
 
 
@@ -72,26 +72,26 @@ class App extends Component
   }
 
   
-  inpHandler = event =>
-  {
-    this.setState({
-      inp: event.target.value
-    })
+  // inpHandler = event =>
+  // {
+  //   this.setState({
+  //     inp: event.target.value
+  //   })
 
    
-  }
+  // }
 
-  deleteChar = (index) =>
-  {
-    const textArr = this.state.inp.split('')
-    textArr.splice(index, 1)
-    const newArr = textArr.join('')
+  // deleteChar = (index) =>
+  // {
+  //   const textArr = this.state.inp.split('')
+  //   textArr.splice(index, 1)
+  //   const newArr = textArr.join('')
 
-    this.setState({
-      inp: newArr
-    })
+  //   this.setState({
+  //     inp: newArr
+  //   })
     
-    }
+  //   }
   
 
 
@@ -99,10 +99,24 @@ class App extends Component
   {
     // Inline Style
     const style = {
-      backgroundColor: 'orange',
+      backgroundColor: 'green',
+      color:'white',
       border: '1px solid blue',
       cursor: 'pointer',
       padding:'8px'
+    }
+
+    // let classes = ["red" ,"bold"].join(" ") // Gives "red bold", 2 css classes combined
+
+    // Alternative dynamic styling
+    let classes = []
+    if (this.state.persons.length <= 2)
+    {
+      classes.push('red') // ['red]
+    }
+    if (this.state.persons.length <= 1)
+    {
+      classes.push('bold') // ['red, 'bold']
     }
 
     let persons = null;
@@ -118,21 +132,21 @@ class App extends Component
             click={() => this.deletePersonHandler(index)}
             inputHandler = {event => this.nameChangeHandler(event, item.id)}
           />
-          </div> 
+        </div> 
+      })
+      style.backgroundColor = 'red'
 
-        })
       }
 
-    const char = this.state.inp.split('').map((i, index) =>
-    {
-      return <CharComp char={i} clicked={() => this.deleteChar(index)}/>
-    })
-      console.log(char)
+    // const char = this.state.inp.split('').map((i, index) =>
+    // {
+    //   return <CharComp char={i} clicked={() => this.deleteChar(index)}/>
+    // })
 
     return (
       <div className="App">
 
-        <p className="App-intro"> React Starter</p>
+        <p className={classes.join(" ")}> React Starter</p>
 
         <button onClick = {this.togglePersons} style={style}> 
           Toggle Persons
@@ -140,16 +154,16 @@ class App extends Component
 
         {persons}
 
-        {char}
+        {/* {char} */}
 
         {/* <UserInput change={this.userNameHandler} value={this.state.userName}/>
 
         <UserOutput name={this.state.userName}/>
         <UserOutput /> */}
 
-        <input onChange={(event) => this.inpHandler(event)}/>
-        <p>{this.state.inp.length}</p>
-        <Validation length={this.state.inp.length} />
+        {/* <input onChange={(event) => this.inpHandler(event)} value={this.state.inp}/> */}
+        {/* <p>{this.state.inp.length}</p> */}
+        {/* <Validation length={this.state.inp.length} /> */}
 
       </div>
     );
