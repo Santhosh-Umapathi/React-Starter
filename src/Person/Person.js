544a0278738830d3cf3c3346ae91694d3f188104
Santhosh Umapathi <Santhosh.professional1@gmail.com>
2020-09-13 08:47:30 +0200

    styled component completed

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,5 +1,19 @@
 import React from 'react';
-import './Person.css'
+// import './Person.css'
+import styled from 'styled-components'
+
+const StyledDiv = styled.div` // CSS without class names
+	width: 80%;
+	box-shadow: 0 2px 3px grey;
+	border: 2px solid orange;
+	text-align: center;
+	padding: 20px;
+	margin: 10px auto;
+	@media (min-width: 500px)
+	{
+		width: 450px
+	}
+`
 
 const Person = (props) =>
 {
@@ -7,13 +21,13 @@ const Person = (props) =>
 	const {name, age, children, inputHandler, click} = props
 
 	return (
-	<div>
+	<StyledDiv>
 		<p className='Person' onClick={click}> I am {name}, {age} years old </p>
 			
 		<p>{children}</p>
 			
 		<input onChange={inputHandler} />
-	</div>
+	</StyledDiv>
 ); 
 }
 
